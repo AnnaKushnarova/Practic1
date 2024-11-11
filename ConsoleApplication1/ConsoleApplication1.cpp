925b3fff1f7f307846ef2dd817e272c7cc0c0928
Anna Kushnarova <annakushn@outlook.com>
2024-11-11 12:20:21 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    double r;
+    int a;
+    short b;
+    float c;
+    SetConsoleOutputCP(1251);
+    cout << "Введіть a-->";
+    cin >> a;
+    cout << "a = " << a << "\n";
+    cout << "Введіть b-->";
+    cin >> b;
+    cout << "b = " << b << "\n";
+    cout << "Введіть c-->";
+    cin >> c;
+    cout << "c = " << c << "\n";
+    if (c == 0) {
+        cout << "Помилка: c не може бути нулем." << endl;
+    }
+    else if (a * a == b) {
+        cout << "Помилка: a * a не може дорівнювати b." << endl;
+    }
+    else {
+        r = (atan(12 / c) + 73) / (a * a - b);
+        cout << "Результат:" << r << endl;
+    }
+    return 0;
+}
